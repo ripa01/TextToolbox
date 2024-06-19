420f2802a3b68ed91d53d5b13233f556fe792779
ripa01 <ripabiswas2739@gmail.com>
2024-06-20 02:46:02 +0600

    added about section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,16 @@
-import './App.css';
-import Navbar from './components/Navbar';
-import Textform from './components/Textform';
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Textform from "./components/Textform";
+import About from "./components/About";
 
 function App() {
   return (
     <>
-    <Navbar title = "TextToolBox" />
-    <Textform heading = 'Enter Your Text To Analyze'/>
+      <Navbar title="TextToolBox" />
+      <Textform heading="Enter Your Text To Analyze" />
+      <About />
 
-   
-    </> 
+    </>
   );
 }
 
---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+export default function About() {
+  return (
+    <div className="items-center justify-between mx-20 p-4">
+      <div className="block w-full p-6 bg-white border border-gray-200 rounded-lg shadow hover:bg-gray-100 dark:bg-gray-800 dark:border-gray-700 dark:hover:bg-gray-700">
+        <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">
+          About us
+        </h1>
+        <p className="font-normal text-gray-700 dark:text-gray-400">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Rem,
+          consectetur. Beatae quasi officia, impedit sed molestias asperiores
+          sapiente ad nam repellendus provident placeat reiciendis numquam,
+          dicta perferendis ipsa. Cupiditate, magni.
+        </p>
+      </div>
+    </div>
+  );
+}
